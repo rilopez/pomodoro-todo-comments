5fc675111ce76ceda88bb18720d348ab1e1e6a5b
Isaac Lopez <ilopez@nearsoft.com>
2013-09-29 21:10:55 +0000

    refactor

--- plugin/src/com/ril/PomodoroCommentVSFListener.java
+++ plugin/src/com/ril/PomodoroCommentVSFListener.java
@@ -2,8 +2,6 @@ package com.ril;
 
 
 import com.intellij.openapi.components.ProjectComponent;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.RangeMarker;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.ui.MessageType;
@@ -16,7 +14,9 @@ import com.intellij.openapi.vfs.VirtualFileEvent;
 import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.WindowManager;
-import com.intellij.psi.*;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
 import com.intellij.psi.search.PsiTodoSearchHelper;
 import com.intellij.psi.search.TodoItem;
 import com.intellij.ui.awt.RelativePoint;
@@ -27,13 +27,12 @@ import java.util.regex.Pattern;
 
 public class PomodoroCommentVSFListener implements ProjectComponent {
 
-    private final Project myProject;
     private static TodoItem activeTodoItem;
+    private final Project myProject;
 
-    public PomodoroCommentVSFListener (Project project) {
+    public PomodoroCommentVSFListener(Project project) {
         myProject = project;
     }
-
 
     private static void showMessage(String message, Project myProject) {
         WindowManager windowManager = WindowManager.getInstance();
@@ -50,6 +49,50 @@ public class PomodoroCommentVSFListener implements ProjectComponent {
                         Balloon.Position.atRight);
     }
 
+    private static TodoItem findActivePomodoroTodoItem(TodoItem[] todoItems) {
+        Pattern pattern = Pattern.compile("\\btodo \\*p[0-9]+\\b.*", Pattern.CASE_INSENSITIVE);
+        TodoItem foundActivePomodoro = null;
+
+        for (TodoItem todoItem : todoItems) {
+            String todoText = getTodoItemText(todoItem);
+            //looks for strings like this TODO *P0: first task
+            Matcher todoMatcher = pattern.matcher(todoText);
+            if (todoMatcher.find()) {
+                foundActivePomodoro = todoItem;
+                break;
+            }
+        }
+        return foundActivePomodoro;
+    }
+
+    private static String getTodoItemText(TodoItem todoItem) {
+        TextRange textRange = todoItem.getTextRange();
+        PsiElement psiElement = todoItem.getFile().findElementAt(textRange.getStartOffset());
+        return psiElement.getText();
+    }
+
+    private static void setActivePomodoro(TodoItem foundActivePomodoro, Project project) {
+        activeTodoItem = foundActivePomodoro;
+        showMessage(String.format("active pomodoro detected: <strong>%s</strong>", getTodoItemText(activeTodoItem)), project);
+    }
+
+    private static void findActivePomodoroInProject(Project myProject) {
+        PsiTodoSearchHelper todoHelper = PsiTodoSearchHelper.SERVICE.getInstance(myProject);
+
+        PsiFile[] filesWithTodoItems = todoHelper.findFilesWithTodoItems();
+
+        TodoItem pomodoroTodoItem = null;
+        for (PsiFile fileWithTodoItem : filesWithTodoItems) {
+            pomodoroTodoItem = findActivePomodoroTodoItem(todoHelper.findTodoItems(fileWithTodoItem));
+            if (pomodoroTodoItem != null) {
+                break;
+            }
+        }
+        if (pomodoroTodoItem != null) {
+            setActivePomodoro(pomodoroTodoItem,myProject);
+        }
+    }
+
     public void projectOpened() {
         ProjectRootManager projectRootManager = ProjectRootManager.getInstance(myProject);
         VirtualFile[] sourceRoots = projectRootManager.getContentSourceRoots();
@@ -58,6 +101,7 @@ public class PomodoroCommentVSFListener implements ProjectComponent {
         // Add the Virtual File listener
         MyVfsListener vfsListener = new MyVfsListener(myProject);
         VirtualFileManager.getInstance().addVirtualFileListener(vfsListener, myProject);
+
 
 
     }
@@ -78,7 +122,6 @@ public class PomodoroCommentVSFListener implements ProjectComponent {
         return "com.ril.PomodoroCommentVSFListener";
     }
 
-
     // -------------------------------------------------------------------------
     // MyVfsListener
     // -------------------------------------------------------------------------
@@ -87,72 +130,26 @@ public class PomodoroCommentVSFListener implements ProjectComponent {
         private Project myProject;
 
         public MyVfsListener(Project project) {
-              this.myProject = project;
+            this.myProject = project;
         }
 
         @Override
         public void contentsChanged(VirtualFileEvent event) {
-            System.out.println("contentsChanged " + event.getFileName());
-            updateTodoCount(myProject);
-
             PsiTodoSearchHelper todoHelper = PsiTodoSearchHelper.SERVICE.getInstance(myProject);
             PsiFile psiFile = PsiManager.getInstance(myProject).findFile(event.getFile());
-            Document document = PsiDocumentManager.getInstance(myProject).getDocument(psiFile);
 
-            CharSequence chars = document.getCharsSequence();
-
-            Pattern pattern = Pattern.compile("\\btodo \\*p[0-9]+\\b.*",Pattern.CASE_INSENSITIVE);
-
-            TodoItem[] todoItems = todoHelper.findTodoItems(psiFile);
-            for (TodoItem todoItem : todoItems) {
-                String todoText = getTodoItemText(document, chars, todoItem);
-
-                //looks for strings like this TODO *P0: first task
-                Matcher todoMatcher = pattern.matcher(todoText);
-
-                if (todoMatcher.find()){
-
-                    showMessage(String.format("active pomodoro detected: <strong>%s</strong>", todoText),myProject);
-                }
+            TodoItem foundActivePomodoro = findActivePomodoroTodoItem(todoHelper.findTodoItems(psiFile));
+            if (foundActivePomodoro != null) {
+                setActivePomodoro(foundActivePomodoro,myProject);
             }
-        }
-
-        private String getTodoItemText(Document document, CharSequence chars, TodoItem todoItem) {
-            TextRange textRange=todoItem.getTextRange();
-
-            int startOffset=textRange.getStartOffset();
-            int endOffset=textRange.getEndOffset();
-            int lineNumber = document.getLineNumber(startOffset);
-            int lineStartOffset = document.getLineStartOffset(lineNumber);
-            int columnNumber=startOffset-lineStartOffset;
-            while(chars.charAt(lineStartOffset) == '\t' || chars.charAt(lineStartOffset)==' '){
-                lineStartOffset++;
-            }
-            int lineEndOffset = document.getLineEndOffset(lineNumber);
-            return chars.subSequence(lineStartOffset, Math.min(lineEndOffset, chars.length())).toString();
         }
 
         public void fileCreated(VirtualFileEvent event) {
-            updateTodoCount(myProject);
-        }
-
-        private void updateTodoCount(Project myProject) {
-
-            int count = 0;
-            PsiTodoSearchHelper todoHelper = PsiTodoSearchHelper.SERVICE.getInstance(myProject);
-
-            PsiFile[] filesWithTodoItems = todoHelper.findFilesWithTodoItems();
-            for (PsiFile filesWithTodoItem : filesWithTodoItems) {
-                count += todoHelper.getTodoItemsCount(filesWithTodoItem);
-            }
-
-            int todoCount = count;
-            System.out.println("TODO count:" + todoCount);
-
+            findActivePomodoroInProject(myProject);
         }
 
         public void fileDeleted(VirtualFileEvent event) {
-            updateTodoCount(myProject);
+            findActivePomodoroInProject(myProject);
         }
     }
 }
