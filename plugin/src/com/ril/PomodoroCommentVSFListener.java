2d615ab0974721eedfa5deaf1781a9c48ca23f1d
Isaac Lopez <ilopez@nearsoft.com>
2013-09-29 20:41:25 +0000

    add matcher for active pomodoro

--- plugin/src/com/ril/PomodoroCommentVSFListener.java
+++ plugin/src/com/ril/PomodoroCommentVSFListener.java
@@ -2,11 +2,14 @@ package com.ril;
 
 
 import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.RangeMarker;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileAdapter;
 import com.intellij.openapi.vfs.VirtualFileEvent;
@@ -15,12 +18,17 @@ import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.psi.*;
 import com.intellij.psi.search.PsiTodoSearchHelper;
+import com.intellij.psi.search.TodoItem;
 import com.intellij.ui.awt.RelativePoint;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PomodoroCommentVSFListener implements ProjectComponent {
 
     private final Project myProject;
+    private static TodoItem activeTodoItem;
 
     public PomodoroCommentVSFListener (Project project) {
         myProject = project;
@@ -86,23 +94,42 @@ public class PomodoroCommentVSFListener implements ProjectComponent {
         public void contentsChanged(VirtualFileEvent event) {
             System.out.println("contentsChanged " + event.getFileName());
             updateTodoCount(myProject);
-        /*
+
             PsiTodoSearchHelper todoHelper = PsiTodoSearchHelper.SERVICE.getInstance(myProject);
             PsiFile psiFile = PsiManager.getInstance(myProject).findFile(event.getFile());
+            Document document = PsiDocumentManager.getInstance(myProject).getDocument(psiFile);
 
-            PsiElementFactory psiFactory = JavaPsiFacade.getInstance(myProject).getElementFactory();
+            CharSequence chars = document.getCharsSequence();
+
+            Pattern pattern = Pattern.compile("\\btodo \\*p[0-9]+\\b.*",Pattern.CASE_INSENSITIVE);
 
             TodoItem[] todoItems = todoHelper.findTodoItems(psiFile);
-            int i = 0;
             for (TodoItem todoItem : todoItems) {
-                System.out.println(todoItem.getTextRange());
-                PsiElement psiTodoComment = psiFile.findElementAt(todoItem.getTextRange().getStartOffset());
-                PsiComment modifiedComment = psiFactory.createCommentFromText(psiTodoComment.getText() + " - " + i, null);
-                psiTodoComment.replace(modifiedComment);
-                i++;
-            }
-*/
+                String todoText = getTodoItemText(document, chars, todoItem);
 
+                //looks for strings like this TODO *P0: first task
+                Matcher todoMatcher = pattern.matcher(todoText);
+
+                if (todoMatcher.find()){
+
+                    showMessage(String.format("active pomodoro detected: <strong>%s</strong>", todoText),myProject);
+                }
+            }
+        }
+
+        private String getTodoItemText(Document document, CharSequence chars, TodoItem todoItem) {
+            TextRange textRange=todoItem.getTextRange();
+
+            int startOffset=textRange.getStartOffset();
+            int endOffset=textRange.getEndOffset();
+            int lineNumber = document.getLineNumber(startOffset);
+            int lineStartOffset = document.getLineStartOffset(lineNumber);
+            int columnNumber=startOffset-lineStartOffset;
+            while(chars.charAt(lineStartOffset) == '\t' || chars.charAt(lineStartOffset)==' '){
+                lineStartOffset++;
+            }
+            int lineEndOffset = document.getLineEndOffset(lineNumber);
+            return chars.subSequence(lineStartOffset, Math.min(lineEndOffset, chars.length())).toString();
         }
 
         public void fileCreated(VirtualFileEvent event) {
@@ -120,7 +147,7 @@ public class PomodoroCommentVSFListener implements ProjectComponent {
             }
 
             int todoCount = count;
-            showMessage("TODO count:" + todoCount, myProject);
+            System.out.println("TODO count:" + todoCount);
 
         }
 
